74ce2fa3a90b06be6adc75485b1266331d7f1a38
Mohit Kuamar <107832471+MohitNSUT@users.noreply.github.com>
2024-08-09 21:44:30 +0530

    Update ApiEndPoint.js

--- Frontend/src/services/ApiEndPoint.js
+++ Frontend/src/services/ApiEndPoint.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://notes-hub-5dd9.onrender.com',
+    baseURL: 'https://notes-app-backend-e3h9.onrender.com',
     headers: {
         'Content-Type': 'application/json' // Removed extra space
     },
