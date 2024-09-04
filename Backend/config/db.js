f9a329142c06770466036cf7c284ab9b8598ad01
Mohit Kuamar <107832471+MohitNSUT@users.noreply.github.com>
2024-09-04 16:24:43 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const DbCon=async()=>{
-        await mongoose.connect("mongodb+srv://mohitgk7185:0Rar2uIbiblDtXv0@cluster0.dlf4r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect("your URL")
         console.log('Mongodb is connected');
 }
 export default DbCon
